727f98847b85c9a3bd3cb0644d6e1a2b8c6b80cf
Stream <Stream_2@qq.com>
2026-03-17 17:57:04 +0800

    refactor: clear SerialName

--- scalable-agent-demo/tool/kotlin-script/contract/src/main/kotlin/ai/dify/stream/tool/kotlinscript/contract/Models.kt
+++ scalable-agent-demo/tool/kotlin-script/contract/src/main/kotlin/ai/dify/stream/tool/kotlinscript/contract/Models.kt
@@ -1,5 +1,6 @@
 package ai.dify.stream.tool.kotlinscript.contract
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -11,12 +12,14 @@ public data class KotlinScriptParams(
 @Serializable
 public sealed interface KotlinScriptResult {
     @Serializable
+    @SerialName("Success")
     public data class Success(
         val returnValue: String,
         val stdout: String,
     ) : KotlinScriptResult
 
     @Serializable
+    @SerialName("Failure")
     public data class Failure(
         val message: String,
         val stdout: String,
--- scalable-agent-demo/tool/shell/contract/src/main/kotlin/ai/dify/stream/tool/shell/contract/ShellCommandResult.kt
+++ scalable-agent-demo/tool/shell/contract/src/main/kotlin/ai/dify/stream/tool/shell/contract/ShellCommandResult.kt
@@ -1,10 +1,12 @@
 package ai.dify.stream.tool.shell.contract
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 public sealed interface ShellCommandResult {
     @Serializable
+    @SerialName("Success")
     public data class Success(
         val exitCode: Int,
         val stdout: String,
@@ -12,12 +14,14 @@ public sealed interface ShellCommandResult {
     ) : ShellCommandResult
 
     @Serializable
+    @SerialName("Timeout")
     public data class Timeout(
         val stdout: String,
         val stderr: String,
     ) : ShellCommandResult
 
     @Serializable
+    @SerialName("Failure")
     public data class Failure(
         val message: String,
     ) : ShellCommandResult
